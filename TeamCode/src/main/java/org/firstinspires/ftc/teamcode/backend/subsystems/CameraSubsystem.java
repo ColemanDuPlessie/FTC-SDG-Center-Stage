56e6aa5c7462b7cd5c7224ce977a377dfa546034
ColemanDuPlessie <coleyduplessie@gmail.com>
2023-12-06 12:11:39 -0500

    Some more camera stuff...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/CameraSubsystem.java
@@ -4,15 +4,15 @@ import android.util.Size;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.backend.cv.TeamShippingElementDetector;
-import org.firstinspires.ftc.teamcode.backend.cv.pipelines.BackdropLocalizationPipeline;
+import org.firstinspires.ftc.teamcode.backend.cv.pipelines.TeamPropDetectionPipeline;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -23,8 +23,6 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.List;
 
-import kotlin.NotImplementedError;
-
 @Config
 public class CameraSubsystem extends SubsystemBase {
 
@@ -34,9 +32,15 @@ public class CameraSubsystem extends SubsystemBase {
     private AprilTagProcessor aprilTag; // For built in ATag stuff
     private VisionPortal visionPortal;
 
-    public enum pipelineType { // TODO redo this from the ground up.
+    public enum pipelineType {
         PROP_DETECTOR,
         BACKDROP_LOCALIZER
+    }
+
+    public enum PROP_POSITION {
+        LEFT,
+        CENTER,
+        RIGHT
     }
 
     pipelineType livePipeline;
@@ -46,13 +50,14 @@ public class CameraSubsystem extends SubsystemBase {
         if (p == pipelineType.BACKDROP_LOCALIZER) {
             initAprilTag(ahwMap);
         } else {
+            initProp(ahwMap);
         }
     }
 
     private void initProp(HardwareMap h) {
         int cameraMonitorViewId = h.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", h.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(h.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
-        camera.setPipeline(new TeamPropDetectionPipeline());
+        camera.setPipeline(new TeamPropDetectionPipeline(1, 822.317/2, 822.317/2, 319.495/2, 242.502/2));
         FtcDashboard.getInstance().startCameraStream(camera, 0);
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -92,6 +97,30 @@ public class CameraSubsystem extends SubsystemBase {
 
         visionPortal.setProcessorEnabled(aprilTag, true);
 
+    }
+
+    public PROP_POSITION getPropPosition() {
+        return PROP_POSITION.CENTER; // TODO
+    }
+
+    public Pose2d getBackdropPosition() { // TODO
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        if (currentDetections.size() == 0) {
+            return null;
+        }
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+        return null;
     }
 
     public void stopStream() {
