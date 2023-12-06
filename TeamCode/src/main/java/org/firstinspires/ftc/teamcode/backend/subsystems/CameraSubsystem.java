eeb140da2bf1874a4dbdfec97611639a7ee47077
ColemanDuPlessie <coleyduplessie@gmail.com>
2023-12-06 12:16:27 -0500

    Cleaned up a little vision code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/CameraSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/CameraSubsystem.java
@@ -111,13 +111,9 @@ public class CameraSubsystem extends SubsystemBase {
 
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
-            } else {
-                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                // detection.id;
+                // detection.metadata.name;
+                // detection.ftcPose
             }
         }
         return null;
